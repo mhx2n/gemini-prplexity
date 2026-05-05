ff9fb2bae86e170cda941ed11b7539d7ca9d40e5
mhx2n <mhdiv2331@gmail.com>
2026-05-05 17:52:09 +0600

    Update prplexity.py

--- prplexity.py
+++ prplexity.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import requests
 import json
 import uuid
@@ -343,4 +344,5 @@ def perplexity_ask():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
